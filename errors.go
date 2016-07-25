6a6cca870c44ad5db6a3a126638dde4ba3c9d32f
Jordan Deitch <jordan.deitch@inturn.co>
2016-07-25 17:19:55 -0400

    error log

--- errors.go
+++ errors.go
@@ -1,8 +1,18 @@
 package helpers
 
+import (
+	"log"
+	"os"
+)
+
 // Simple Panic on error
 func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+// Prints to stderr
+func errorLog(e ...interface{}) {
+	log.New(os.Stderr, "", 0).Println(e)
 }
