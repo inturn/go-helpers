dd200ea7ecaf40ef7589c65d82629389acba1a44
Jordan Deitch <jordan.deitch@inturn.co>
2016-11-09 14:44:13 -0500

    errors

--- errors.go
+++ errors.go
@@ -3,16 +3,24 @@ package helpers
 import (
 	"log"
 	"os"
+	"fmt"
 )
 
 // Simple Panic on error
-func Check(e error) {
+func ErrorCheckAndPanic(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+func ErrorCheckAndPrint(e error) {
+	if e != nil {
+		fmt.Println("ERROR: " + e.Error())
+	}
+}
+
+
 // Prints to stderr
-func errorLog(e ...interface{}) {
-	log.New(os.Stderr, "", 0).Printf("%+v",e)
+func ErrorLog(e ...interface{}) {
+	log.New(os.Stderr, "", 0).Printf("%+v", e)
 }
